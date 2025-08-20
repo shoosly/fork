d96f663198742dd8c51c79b580198d631077127d
Shoosly <shoosly@gmail.com>
2025-08-21 02:58:48 +0330

    change value of i

--- src/main/java/org/shoosly/Main.java
+++ src/main/java/org/shoosly/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-
+             i+=i;
             System.out.println("i = " + i);
         }
     }
