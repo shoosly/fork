9b3fb5742d28ee4a2348b3721f87e454c473ccfe
Shoosly <shoosly@gmail.com>
2025-08-21 03:27:55 +0330

    cleare

--- src/main/java/org/shoosly/Main.java
+++ src/main/java/org/shoosly/Main.java
@@ -7,7 +7,6 @@ public class Main {
         System.out.print("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-            int k;
             System.out.println("i = " + i);
         }
     }
