b1fd86aaf92be367bd5a80ff0e90b784838b3c81
Shoosly <shoosly@gmail.com>
2025-08-21 03:16:04 +0330

    change printf to print

--- src/main/java/org/shoosly/Main.java
+++ src/main/java/org/shoosly/Main.java
@@ -4,7 +4,7 @@ package org.shoosly;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
              i+=i;
